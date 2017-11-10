fa0c6cdbb1011325b9b6a8e008a9fa03a518aa7e
louis cochinho <louis.cochinho@hotmail.fr>
2017-11-10 15:16:46 +0100

    ADD NIGHT dans dispo

--- javaee7-angular/src/main/java/com/cortez/samples/javaee7angular/data/Disponibility.java
+++ javaee7-angular/src/main/java/com/cortez/samples/javaee7angular/data/Disponibility.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Disponibility {
 	
     public enum Periode {
-        MORNING, MIDDAY, EVENING
+        MORNING, MIDDAY, EVENING, NIGHT
     }
     
     public enum Day {
