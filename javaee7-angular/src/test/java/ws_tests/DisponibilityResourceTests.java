b337e9eba6f706d1139fad5c8e7b2293cfdd4217
louis cochinho <louis.cochinho@hotmail.fr>
2017-11-27 20:21:44 +0100

    tentative ecriture junit failed

--- javaee7-angular/src/test/java/ws_tests/DisponibilityResourceTests.java
+++ javaee7-angular/src/test/java/ws_tests/DisponibilityResourceTests.java
@@ -2,9 +2,34 @@ package ws_tests;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.cortez.samples.javaee7angular.data.Disponibility;
+import com.cortez.samples.javaee7angular.data.Restaurant;
+
 public class DisponibilityResourceTests {
+
+	private static EntityManager em;
+	private static EntityManagerFactory emf;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("persistenceUnit");
+		em = emf.createEntityManager();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		em.close();
+		emf.close();
+	}
 
 	@Test
 	public void testGetDisponibility() {
@@ -13,7 +38,19 @@ public class DisponibilityResourceTests {
 
 	@Test
 	public void testSaveDisponibility() {
-		fail("Not yet implemented");
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		// Ajout restaurant
+		Restaurant rest = new Restaurant();
+		rest.setName("restoTest");
+		em.persist(rest);
+		tx.commit();
+		long restId = rest.getId();
+		assertNotNull("Id not null", restId);
+		Restaurant restRetrouve = em.find(Restaurant.class, restId);
+		assertNotNull("Restaurant from database", restRetrouve);
+		assertEquals("getName", "restoTest", restRetrouve.getName());
 	}
 
 	@Test
