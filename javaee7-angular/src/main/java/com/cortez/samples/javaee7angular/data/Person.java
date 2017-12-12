ac3ac4fe4989ba154e2414763ce8809ed731a0a4
lucas <Lucas.guery@gmail.com>
2017-12-12 12:39:44 +0100

    Ajout des table person customer et Restaurant manager avec heritage

--- javaee7-angular/src/main/java/com/cortez/samples/javaee7angular/data/Person.java
+++ javaee7-angular/src/main/java/com/cortez/samples/javaee7angular/data/Person.java
@@ -1,66 +1,68 @@
 package com.cortez.samples.javaee7angular.data;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 /**
  * Entity implementation class for Entity: Restaurant
  *
  */
-//@Entity
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "PERSON_TYPE", discriminatorType = DiscriminatorType.STRING)
 public class Person {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private Long id;
-//
-//	private String first_name;
-//	private String last_name;
-//	private String tel_number;
-//	private String email;
-//	
-//	public Person(){
-//		super();
-//	}
-//	
-//	public void setFirstName(String first_name) {
-//		this.first_name = first_name;
-//	}
-//
-//	public String getFirstName() {
-//		return this.first_name;
-//	}
-//	
-//	public void setLastName(String last_name) {
-//		this.last_name = last_name;
-//	}
-//
-//	public String getLastName() {
-//		return this.last_name;
-//	}
-//	
-//	public String getTel_number() {
-//		return this.tel_number;
-//	}
-//
-//	public void setTel_number(String tel_number) {
-//		this.tel_number = tel_number;
-//	}
-//
-//	public String getEmail() {
-//		return this.email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String first_name;
+	private String last_name;
+	private String tel_number;
+	private String email;
+	
+	public Person(){
+		super();
+	}
+	
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getFirstName() {
+		return this.first_name;
+	}
+	
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getLastName() {
+		return this.last_name;
+	}
+	
+	public String getTel_number() {
+		return this.tel_number;
+	}
+
+	public void setTel_number(String tel_number) {
+		this.tel_number = tel_number;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
