19b2d3d3c90999dc49f8bafd36008955ff57a169
lambertrocher <lambert.rocher@gmail.com>
2017-12-18 15:44:12 +0100

    forms validation

--- my-app/src/app/inscription-connexion/inscription-connexion.component.ts
+++ my-app/src/app/inscription-connexion/inscription-connexion.component.ts
@@ -17,17 +17,62 @@ export class InscriptionConnexionComponent implements OnInit {
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
-  phone = new FormControl('', [Validators.required, Validators.max(10)]);
+  email2 = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
+
+  required = new FormControl('', [Validators.required]);
+
+  required2 = new FormControl('', [Validators.required]);
+
+  required3 = new FormControl('', [Validators.required]);
+
+  required4 = new FormControl('', [Validators.required]);
+
+  required5 = new FormControl('', [Validators.required]);
+
+  required6 = new FormControl('', [Validators.required]);
+
+
+  getErrorMessageEmail() {
     return this.email.hasError('required') ? 'You must enter a value' :
       this.email.hasError('email') ? 'Not a valid email' :
         '';
   }
 
-  getErrorMessagePhone() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('phone') ? 'Not a valid phone number' :
+  getErrorMessageEmail2() {
+    return this.email2.hasError('required') ? 'You must enter a value' :
+      this.email2.hasError('email') ? 'Not a valid email' :
         '';
+  }
+
+  getErrorMessageRequired() {
+    return this.required.hasError('required') ? 'You must enter a value' :
+        '';
+  }
+
+  getErrorMessageRequired2() {
+    return this.required2.hasError('required') ? 'You must enter a value' :
+      '';
+  }
+
+  getErrorMessageRequired3() {
+    return this.required3.hasError('required') ? 'You must enter a value' :
+      '';
+  }
+
+
+  getErrorMessageRequired4() {
+    return this.required4.hasError('required') ? 'You must enter a value' :
+      '';
+  }
+
+  getErrorMessageRequired5() {
+    return this.required5.hasError('required') ? 'You must enter a value' :
+      '';
+  }
+
+  getErrorMessageRequired6() {
+    return this.required6.hasError('required') ? 'You must enter a value' :
+      '';
   }
 }
